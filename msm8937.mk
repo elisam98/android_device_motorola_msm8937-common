f507f505d97cbb4c341f27411cbd5a9faa700e45
elisam98 <cornbeefonrye@hotmail.com>
2019-04-18 09:20:22 -0400

    Revert "Pulled media codecs & profiles from stock + kept ffmpeg"
    This reverts commit b0eeb451a2989e7a9e9e80086784e48827a9b70f.

--- msm8937.mk
+++ msm8937.mk
@@ -20,10 +20,8 @@ $(call inherit-product, device/motorola/qcom318-32/qcom318-32.mk)
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml \
-    $(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs_8956.xml \
     $(LOCAL_PATH)/configs/media_codecs_performance.xml:system/etc/media_codecs_performance.xml \
-    $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml \
-    $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles_8956.xml
+    $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml
 
 # Ramdisk
 PRODUCT_PACKAGES += \
